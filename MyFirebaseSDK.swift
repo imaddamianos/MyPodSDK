16ea72a76c07349a06dd64222181b09fbdf5973b
iMad <damianos.imad@gmail.com>
2023-07-13 02:15:00 +0300

    firebase

--- MyFirebaseSDK.swift
+++ MyFirebaseSDK.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import Firebase
+
+public class MyFirebaseSDK {
+    public static let shared = MyFirebaseSDK()
+    
+    private init() {
+        // Private initializer to enforce singleton pattern
+    }
+    
+    public func initialize() {
+        FirebaseApp.configure()
+        // Initialize Crashlytics
+        Crashlytics.crashlytics().setCrashlyticsCollectionEnabled(true)
+        // Initialize Remote Config
+        RemoteConfig.remoteConfig().fetchAndActivate(completionHandler: nil)
+        // Initialize Analytics
+        Analytics.setAnalyticsCollectionEnabled(true)
+    }
+}
+
