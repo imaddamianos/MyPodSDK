3715856e3a8f77c2d95c9f5f0e14ee5c481e150c
iMad <damianos.imad@gmail.com>
2023-07-12 21:01:05 +0300

    Initial Commit

---
+++ MyFirebaseSDK/MyFirebaseSDK.h
@@ -0,0 +1,18 @@
+//
+//  MyFirebaseSDK.h
+//  MyFirebaseSDK
+//
+//  Created by iMad on 12/07/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyFirebaseSDK.
+FOUNDATION_EXPORT double MyFirebaseSDKVersionNumber;
+
+//! Project version string for MyFirebaseSDK.
+FOUNDATION_EXPORT const unsigned char MyFirebaseSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFirebaseSDK/PublicHeader.h>
+
+
